c0342ba948c39bc92a39d5888de8400ae0132d90
Marcos Guzman <marcosgtcs@tatasolutioncenter.onmicrosoft.com>
2023-11-14 21:21:51 +0000

    TV Show

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -5,17 +5,17 @@ import { BASE_URL, API_KEY_PARAM } from "../config";
 
 export class TVShowAPI {
     static async fetchPopulars() {
-        const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
+        const response = await axios.get(`${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY_PARAM}`);
         return response.data.results;
     }
 
     static async fetchRecommendations(tvShowId) {
-        const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`);
+        const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations?api_key=${process.env.REACT_APP_API_KEY_PARAM}`);
         return response.data.results;
     }
 
     static async fetchSearchTVShow(title) {
-        const response = await axios.get(`${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`);
+        const response = await axios.get(`${BASE_URL}search/tv?api_key=${process.env.REACT_APP_API_KEY_PARAM}&query=${title}`);
         return response.data.results;
     }
 }
--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=9fff4483308e95486bb9573d04bc101f";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
 
-export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
+export { BASE_URL, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
